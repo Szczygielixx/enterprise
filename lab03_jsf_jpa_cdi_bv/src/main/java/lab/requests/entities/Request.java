6ad4875447c3b53bea7947048e0c2d8f4c862a0a
Szczygielixx <Natalia.Szczygielska.44@gmail.com>
2025-06-20 16:00:36 +0200

    lab03 added

---
+++ lab03_jsf_jpa_cdi_bv/src/main/java/lab/requests/entities/Request.java
@@ -0,0 +1,44 @@
+package lab.requests.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+@Entity
+public class Request {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private LocalDate requestDate;
+
+    @Size(min=3, max=60, message = "{request.size}")
+    private String requestText;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(LocalDate requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public String getRequestText() {
+        return requestText;
+    }
+
+    public void setRequestText(String requestText) {
+        this.requestText = requestText;
+    }
+} 
