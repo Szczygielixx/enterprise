e35bf2ee26c48d0c11d474ab741ff00b7b719896
Szczygielixx <Natalia.Szczygielska.44@gmail.com>
2025-06-21 13:54:51 +0200

    lab 07 added

---
+++ lab07_asp_web_api_ajax/WebApiAjax/CalcResult.cs
@@ -0,0 +1,7 @@
+public class CalcResult
+{
+    public int Sum { get; set; }
+    public int Difference { get; set; }
+    public int Product { get; set; }
+    public int Quotient { get; set; }
+} 
